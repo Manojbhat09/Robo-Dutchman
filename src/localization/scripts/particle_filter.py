f7bbb5af7be0b61e59f47a6f866cb9b319ed89d2
Oshadha <oshadha123@gmail.com>
2020-02-18 15:42:17 -0500

    Working Particle Filter

--- src/localization/scripts/particle_filter.py
+++ src/localization/scripts/particle_filter.py
@@ -1,12 +1,12 @@
+#!/usr/bin/env python
 import matplotlib.pyplot as plt
 import numpy as np
+import rospy
 
+from std_msgs.msg import String
 from filterpy.monte_carlo import systematic_resample
 from scipy.stats import norm
-from numpy.random import uniform, randn
-
-# globals
-error_val = 100
+from numpy.random import uniform, randn, random
 
 # Particle Filter based on the following repository:
 # https://github.com/rlabbe/Kalman-and-Bayesian-Filters-in-Python/blob/master/12-Particle-Filters.ipynb
@@ -14,19 +14,22 @@ error_val = 100
 # wall vertices are [0,0], [0,L1], [L2,0]
 
 class ParticleFilter(object):
-    def __init__(self, N, sensor_locations=None, sensor_std_error=.1,
+    def __init__(self, N, sensor_locations=None, sensor_max=None,
+                 wall_lengths=[20,20], sensor_std_error=.01,
                  initial_x=None):
 
         # initialize attributes
         self.sensor_locations = sensor_locations
+        self.sensor_max = sensor_max
         self.sensor_std_error = sensor_std_error
+        self.wall_lengths = wall_lengths
 
         # initialize particles and weights
         if initial_x is not None:
             self.particles = self.create_gaussian_particles(
                 mean=initial_x, std=(0.5, 0.5, np.pi/4), N=N)
         else:
-            self.particles = self.create_uniform_particles((0,20), (0,20), (-3.14159, 3.14159), N)
+            self.particles = self.create_uniform_particles((0,-20), (0,20), (-3.14159, 3.14159), N)
         
         self.weights = np.ones(N) / N
         
@@ -66,37 +69,14 @@ class ParticleFilter(object):
         global error_val
         
         N = len(self.particles)
-
         for i in range(N):
-            # get particle x, y, th
-            particle = self.particles[i]
-            x = self.particles[i,0]
-            y = self.particles[i,1]
-            th = self.particles[i,2]
-
+            pos = self.particles[i]
             for j, sensor_loc in enumerate(self.sensor_locations):
-                # initialize rotation matrix
-                R = np.array([[np.cos(th), -np.sin(th)],
-                              [np.sin(th), np.cos(th)]])
-
-                # rotate sensor_loc 
-                sensor_loc = R.dot(np.array(sensor_loc))
-
-                # project onto walls (check to ensure sensor will hit wall)
-                proj_wall1 = self.project_vector(np.array([x, 0]), sensor_loc)
-                wall1_dist = np.average(proj_wall1 / sensor_loc) 
-                if wall1_dist < 0:
-                    wall1_dist = error_val
-
-                proj_wall2 = self.project_vector(np.array([0, -y]), sensor_loc)
-                wall2_dist = np.average(proj_wall2 / sensor_loc) 
-                if wall2_dist < 0:
-                    wall2_dist = error_val
+                # find estimated distance from sensors
+                dist = self.find_sensor_dist(pos, sensor_loc)
 
                 # find new posterior probability
-                # distance = np.array([[wall1_dist], [wall2_dist]])
-                self.weights[i] *= norm(wall1_dist, self.sensor_std_error).pdf(z[j])
-                self.weights[i] *= norm(wall2_dist, self.sensor_std_error).pdf(z[j])
+                self.weights[i] *= norm(dist, self.sensor_std_error).pdf(z[j])
 
         self.weights += 1.e-300      # avoid round-off to zero
         self.weights /= sum(self.weights) # normalize
@@ -107,9 +87,13 @@ class ParticleFilter(object):
         pos = self.particles[:, 0:2]
         mean = np.average(pos, weights=self.weights, axis=0)
         var  = np.average((pos - mean)**2, weights=self.weights, axis=0)
-        return mean, var
+
+        max_ind = np.argmax(self.weights)
+        best_est = self.particles[max_ind]
+
+        return [best_est, mean, var]
     
-    def simple_resample(self):
+    def resample_simple(self):
         N = len(self.particles)
         cumulative_sum = np.cumsum(self.weights)
         cumulative_sum[-1] = 1. # avoid round-off error
@@ -119,48 +103,158 @@ class ParticleFilter(object):
         self.particles[:] = self.particles[indexes]
         self.weights.fill(1.0 / N)
 
+    def resample_syst(self):
+        indexes = systematic_resample(self.weights)
+        self.resample_from_index(indexes)
         
     def resample_from_index(self, indexes):
         self.particles[:] = self.particles[indexes]
         self.weights[:] = self.weights[indexes]
         self.weights.fill(1.0 / len(self.weights))
-    
-    # projects vec1 onto vec2
-    def project_vector(self, vec1, vec2):
-        return vec2 * np.dot(vec1, vec2) / np.dot(vec2, vec2)
 
     def neff(self):
         return 1. / np.sum(np.square(self.weights))  
 
-    def update_step(self, z, u):
+    def update_step(self, z, u, plot_points=False):
         N = len(self.particles)
 
         # predict step
-        print("predict")
-        self.predict(u=u, std=(.2, .05))
+        self.predict(u=u, std=(.1, .05))
         
         # update step
-        print("update")
         self.update(z=z)
         
         # resample if too few effective particles
         if self.neff() < N/2:
-            indexes = systematic_resample(self.weights)
-            self.resample_from_index(indexes)
-            assert np.allclose(self.weights, 1/N)
+            self.resample_simple()
+            # assert np.allclose(self.weights, 1/N)
 
         # estimate mean and variance
-        mu, var = self.estimate()
-        return mu, var
+        est = self.estimate()
+        best_est = est[0]
+
+        if plot_points == True:
+            # plot particles and mean
+            plt.scatter(self.particles[:, 0], self.particles[:, 1], 
+                            color='k', marker=',', s=1)
+
+        plt.scatter(best_est[0], best_est[1], marker='s', color='r')
+        return best_est
+
+    # returns scalar projection of vec1 onto vec2
+    def vector_project(self, vec1, vec2):
+        return np.dot(vec1, vec2) / np.linalg.norm(vec2)
+
+    # returns cos(angle) between vec1 and vec2
+    def vector_angle(self, vec1, vec2):
+        return np.dot(vec1, vec2) / (np.linalg.norm(vec1) * np.linalg.norm(vec2))
+
+    # returns distance from sensor to walls given robot position and sensor location
+    def find_sensor_dist(self, pos, sensor_loc):
+        # extract values from pos
+        x = pos[0]
+        y = pos[1]
+        th = pos[2]
+        
+        # find split, min, and max angles
+        L1 = self.wall_lengths[0]
+        L2 = self.wall_lengths[1]
+        split_ang = np.arctan2(-y,-x)
+        max_ang = np.arctan2(L1-y,-x)
+        min_ang = np.arctan2(-y,-L2-x)
+        
+        # initialize rotation matrix
+        R = np.array([[np.cos(th), -np.sin(th)],
+                      [np.sin(th), np.cos(th)]])
+
+        # rotate sensor_loc 
+        sensor_loc = R.dot(np.array(sensor_loc))
+
+        # determine measured distance
+        dist = None           
+        sensor_ang = np.arctan2(sensor_loc[1], sensor_loc[0])
+        if (sensor_ang <= min_ang or sensor_ang >= max_ang):
+            dist = error_val
+        elif (sensor_ang > split_ang):
+            cos_ang = self.vector_angle(sensor_loc, np.array([x,0]))
+            dist = x / cos_ang
+        else:
+            cos_ang = self.vector_angle(sensor_loc, np.array([0, -y]))
+            dist = y / cos_ang
+
+        return dist
+
+class IRSensorArray(object):
+    def __init__(self, N, wall_lengths, sensor_locations, sensor_max, initial_x):
+        # initialize attributes
+        self.sensor_max = sensor_max
+        self.ir_vals = None
+
+        # initialize particle filter        
+        self.filter = ParticleFilter(ParticleFilter(N, sensor_locations=sensor_locations, 
+                                                    sensor_max=sensor_max, wall_lengths=wall_lengths, 
+                                                    initial_x=initial_x))
+
+        # initialize ROS
+        rospy.init_node('listener', anonymous=True)
+        rospy.Subscriber("ir_all", String, self.callback)
+
+        # run particle filter update as fast as it can calculate
+        rate = rospy.Rate(10)
+        while not rospy.is_shutdown():
+            sensor_readings = self.get_sensor_readings()
+            best_est = pf.update_step(sensor_readings, (0,0))
+
+            rospy.loginfo(best_est)
+            rate.sleep()
+
+    def callback(self, data):
+        msg = data.data
+        self.vals = msg.strip().split(' ')
+        self.vals = [float(x) for x in self.vals]
+
+    def get_sensor_readings(self):
+        global error_val
+
+        sensor_readings = []
+        for i, val in enumerate(self.vals):
+            if val > self.sensor_max[i]:
+                sensor_readings.append(error_val)
+            else:
+                sensor_readings.append(val / 1000)
+
+        return sensor_readings
 
 if __name__ == '__main__':
     global error_val
+    error_val = 10000
 
-    sensor_locations = ([0,1], [1,0], [0,-1])
-    sensor_readings = [error_val, 5, 5]
+    N = 100
+    wall_lengths = [20, 20]
+    sensor_locations = ([0,-1], [1,-1], [1,0], [1,1], [0,1])
+    sensor_max = [50, 50, 50, 50, 50]
+    initial_x = None
 
-    pf = ParticleFilter(5000, sensor_locations=sensor_locations)
+    test = True
 
-    for iter in range(10):
-      mu, var = pf.update_step(sensor_readings, (0,0))
-      print(mu, var)
+    if test:
+        plt.figure()
+
+        sensor_locations = ([0,1], [1,0], [0,-1])
+        sensor_readings = [5, 5, error_val]
+        initial_x = [-5, 5, 0]
+
+        pf = ParticleFilter(N, sensor_locations=sensor_locations, sensor_max=sensor_max, initial_x=initial_x)
+
+        for iter in range(20):
+            best_est = pf.update_step(sensor_readings, (0,0))
+            print(best_est)
+
+        d1 = pf.find_sensor_dist(best_est, sensor_locations[0])
+        d2 = pf.find_sensor_dist(best_est, sensor_locations[1])
+        d3 = pf.find_sensor_dist(best_est, sensor_locations[2])
+        print("validation dists: %f %f %f" %(d1, d2, d3))
+
+        plt.show()
+    else:
+        sensor_array = IRSensorArray(N, wall_lengths, sensor_locations, sensor_max, initial_x)
