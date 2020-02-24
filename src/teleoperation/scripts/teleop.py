7f77a49e2eed002241b24526f2e51d7f0eb9c117
Oshadha <oshadha123@gmail.com>
2020-02-24 14:20:39 -0500

    Working Hebi teleop control

--- src/teleoperation/scripts/teleop.py
+++ src/teleoperation/scripts/teleop.py
@@ -5,6 +5,9 @@ from sensor_msgs.msg import Joy, JointState
 from hebiros.srv import EntryListSrv, AddGroupFromNamesSrv, SizeSrv
 from std_msgs.msg import String
 
+global GROUP_NAME, FAMILY_NAME, NAME_1, NAME_2, NAME_3, NAME_4
+global MODE_ARM, MODE_WRIST, DEFAULT_MODE
+
 GROUP_NAME = "RoboDutchmanArm"
 FAMILY_NAME = "RoboDutchman"
 NAME_1 = "Shoulder"
@@ -12,17 +15,49 @@ NAME_2 = "Elbow"
 NAME_3 = "Wrist1"
 NAME_4 = "Wrist2"
 
+MODE_ARM = True
+MODE_WRIST = False
+DEFAULT_MODE = MODE_ARM
+
 class TeleopNode(object):
     def __init__(self):
+        global GROUP_NAME, FAMILY_NAME, NAME_1, NAME_2, NAME_3, NAME_4
+        global MODE_ARM, MODE_WRIST, DEFAULT_MODE
+        
+        # initialize attributes
+        self.hebi_fb = None
+        self.hebi_cmd = None
+
+        self.joy_msg_prev = None
+        self.joy_msg = None
+
+        self.recievedFirstHebiFb = False
+        self.recievedFirstJoyFb = False
+        self.mode = DEFAULT_MODE
+
+        self.btn_mode = 0
+        self.axis_primary = 1
+        self.axis_secondary = 4
+        self.primary_actuator = None
+        self.secondary_actuator = None
+        self.primary_scale = None
+        self.secondary_scale = None
+        self.scale = [1.0/500, 1.0/500, 1.0/50, 1.0/50]
+
+        self.primary_actuator = 0
+        self.primary_scale = self.scale[0]
+        self.secondary_actuator = 1
+        self.secondary_scale = self.scale[1]
         
         # initialize node
         rospy.init_node('teleop', anonymous=True)
 
         # initialize publishers
-        self.cmd_pub = rospy.Publisher('/hebiros/' + GROUP_NAME + '/command/joint_state', JointState, queue_size=10)
+        self.hebi_cmd_pub = rospy.Publisher('/hebiros/' + GROUP_NAME + '/command/joint_state', JointState, queue_size=10)
 
         # initialize subscribers
         rospy.Subscriber('joy', Joy, self.joy_cb)
+        rospy.Subscriber('/hebiros/' + GROUP_NAME + '/feedback/joint_state', JointState, self.hebi_fb_cb)
 
         # initialize services
         self.entry_list_client = rospy.ServiceProxy('/hebiros/entry_list', EntryListSrv)
@@ -32,10 +67,68 @@ class TeleopNode(object):
         self.hebi_lookup()
 
         # spin
-        rospy.spin()    
+        rate = rospy.Rate(200) # 10hz
+        while not rospy.is_shutdown():
+            self.step()
+            rate.sleep()
+
+    def step(self):
+        # Don't do anything until we have heard from
+        # HEBI and the joysticks
+        if (not self.recievedFirstHebiFb or not self.recievedFirstJoyFb): return
+
+        # Update target hebi positions from primary and secondary actuators
+        pos = list(self.hebi_cmd.position)
+
+        pos[self.primary_actuator] = self.hebi_cmd.position[self.primary_actuator] + \
+            self.joy_msg.axes[self.axis_primary] * self.primary_scale            
+
+        pos[self.secondary_actuator] = self.hebi_cmd.position[self.secondary_actuator] + \
+            self.joy_msg.axes[self.axis_secondary] * self.secondary_scale            
+
+        self.hebi_cmd.position = tuple(pos)
+
+        # Send hebi command
+        self.hebi_cmd_pub.publish(self.hebi_cmd)
+
+    def hebi_fb_cb(self, msg):
+        self.hebi_fb = msg
+        if not self.recievedFirstHebiFb:
+            self.hebi_cmd = msg
+            self.hebi_cmd.effort = []
+            self.hebi_cmd.velocity = []
+            self.recievedFirstHebiFb = True
 
     def joy_cb(self, msg):
-        pass
+        global MODE_ARM, MODE_WRIST, DEFAULT_MODE
+        
+        self.joy_msg_prev = self.joy_msg
+        self.joy_msg = msg
+
+        if not self.recievedFirstJoyFb:
+            self.joy_msg_prev = msg
+            self.recievedFirstJoyFb = True
+        
+        # Detect mode switch on button edge
+        if self.joy_msg.buttons[self.btn_mode] \
+                and not self.joy_msg_prev.buttons[self.btn_mode]: 
+
+            self.mode = not self.mode
+
+            # Set primary and secondary actuators and scales
+            # based on current mode
+            if (self.mode == MODE_ARM):
+                self.primary_actuator = 0
+                self.primary_scale = self.scale[0]
+
+                self.secondary_actuator = 1
+                self.secondary_scale = self.scale[1]
+            elif (self.mode == MODE_WRIST):
+                self.primary_actuator = 2
+                self.primary_scale = self.scale[2]
+
+                self.secondary_actuator = 3
+                self.secondary_scale = self.scale[3]
 
     def call_entry_list(self):
         entry_resp = self.entry_list_client.call()
